e872ee321a5fecc986947467b063e33ad60ec4b2
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-11 00:23:46 +0600

    new section added in services

--- src/Pages/Home/Home/Services/Services.jsx
+++ src/Pages/Home/Home/Services/Services.jsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react";
 import Service from "./Service";
+import { FcOvertime } from "react-icons/fc";
 
 const Services = () => {
 
-    const[services,setServices] = useState([])
+    const [services, setServices] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('services.json')
-        .then(res => res.json())
-        .then(datas => setServices(datas))
-    },[])
+            .then(res => res.json())
+            .then(datas => setServices(datas))
+    }, [])
 
     return (
         <div>
@@ -23,6 +24,32 @@ const Services = () => {
                 {
                     services?.map(service => <Service key={service._id} service={service}></Service>)
                 }
+            </div>
+
+            <button className='px-5 py-3 border-2 rounded-md border-orange-500 text-orange-600 hover:bg-orange-700 hover:text-white mx-auto block'>More Services</button>
+
+            <div className="my-8 text-white bg-black flex justify-between p-16 rounded-md">
+                <div className="flex items-center gap-3">
+                    <FcOvertime className="h-12 w-12 text-red-500"/>
+                    <div>
+                        <p>We are open monday-friday</p>
+                        <p className="text-2xl font-semibold">7:00 am - 9:00 pm</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <div>
+                        <p>Have a question?</p>
+                        <p className="text-3xl font-semibold">+2546 251 2658</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <div>
+                        <p>Need a repair? our address</p>
+                        <p className="text-3xl font-semibold">Liza Street, New York</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
