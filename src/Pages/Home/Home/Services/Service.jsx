dfa3288c7a6a8573c6528539f7212b40299d517f
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-10 23:23:19 +0600

    services done

--- src/Pages/Home/Home/Services/Service.jsx
+++ src/Pages/Home/Home/Services/Service.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Service = () => {
+const Service = ({service}) => {
+    const {title,img,price} = service
     return (
-        <div>
-            
+        <div className='border-2 p-5 rounded-md space-y-5 my-9'>
+            <img className='rounded-md w-[314px] h-[208.01px]' src={img} alt="" />
+            <p className='text-2xl font-bold'>{title}</p>
+            <p className='text-xl font-semibold text-red-500'>Price : ${price}</p>
         </div>
     );
 };
--- src/Pages/Home/Home/Services/Services.jsx
+++ src/Pages/Home/Home/Services/Services.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import Service from "./Service";
+
 const Services = () => {
+
+    const[services,setServices] = useState([])
+
+    useEffect(()=>{
+        fetch('services.json')
+        .then(res => res.json())
+        .then(datas => setServices(datas))
+    },[])
+
     return (
         <div>
             <div className='space-y-6'>
@@ -7,8 +19,10 @@ const Services = () => {
                 <p className='text-[#737373] text-center'>the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable.</p>
             </div>
 
-            <div>
-                
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {
+                    services?.map(service => <Service key={service._id} service={service}></Service>)
+                }
             </div>
         </div>
     );
