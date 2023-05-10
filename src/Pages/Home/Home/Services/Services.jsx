49cc1fc643bab3651bc800633c613606f2c8bcb1
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-10 22:53:53 +0600

    services data added

--- src/Pages/Home/Home/Services/Services.jsx
+++ src/Pages/Home/Home/Services/Services.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Services = () => {
     return (
         <div>
@@ -7,6 +5,10 @@ const Services = () => {
                 <p className='text-xl text-center text-orange-600 font-bold'>Service</p>
                 <p className='font-bold text-3xl text-center lg:text-6xl'>Our Service Area</p>
                 <p className='text-[#737373] text-center'>the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable.</p>
+            </div>
+
+            <div>
+                
             </div>
         </div>
     );
