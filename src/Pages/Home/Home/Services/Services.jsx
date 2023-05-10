fa4d81000ec5810a1b0b25061924a40ffbb750d9
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-10 22:51:37 +0600

    Services title, some text added

---
+++ src/Pages/Home/Home/Services/Services.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <div className='space-y-6'>
+                <p className='text-xl text-center text-orange-600 font-bold'>Service</p>
+                <p className='font-bold text-3xl text-center lg:text-6xl'>Our Service Area</p>
+                <p className='text-[#737373] text-center'>the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
