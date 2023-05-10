c0cda8b924214ed41165e1cb78c7ff528e79af4c
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-11 02:52:58 +0600

    erroe page done

---
+++ src/Pages/Shared/Error/Error.jsx
@@ -0,0 +1,11 @@
+import errorImg from '../../../assets/images/error/Frame.png'
+
+const Error = () => {
+    return (
+        <div className='w-full h-[100vh] lg:w-2/3 mx-auto my-auto'>
+            <img src={errorImg} alt="" />
+        </div>
+    );
+};
+
+export default Error;
--- src/Rotes/Routes.jsx
+++ src/Rotes/Routes.jsx
@@ -3,11 +3,13 @@ import Main from "../Layouts/Main";
 import Home from "../Pages/Home/Home/Home/Home";
 import Login from "../Pages/Register-Login/Login/Login";
 import Register from "../Pages/Register-Login/Register/Register";
+import Error from "../Pages/Shared/Error/Error";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Main></Main>,
+        errorElement:<Error></Error>,
         children:[
             {
                 path:"/",
